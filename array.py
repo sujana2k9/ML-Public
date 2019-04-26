fab0c213a3c47dcb170feea1ce77b7ed84bb0f8a
sujana2k9 <49792929+sujana2k9@users.noreply.github.com>
2019-04-26 21:49:59 +0530

    Update array.py

--- array.py
+++ array.py
@@ -1,5 +1,5 @@
 import numpy as np
-'''a=np.array([2,3,6,7,8], dtype=int)
+a=np.array([2,3,6,7,8], dtype=int)
 print(a)
 b=np.array((2,3,4,5), dtype=int)
 print(b)
@@ -10,7 +10,7 @@ print(b)
 b=np.array((2,3,4,5), dtype=int)
 c=np.exp(b)
 print(c)
-print(b[1])'''
+print(b[1])
 d= np.array([[1,2,3,4,5,6,7], [4,5,6,7,8,9,2], [1,2,3,4,5,6,7], [4,5,6,7,8,9,2]], dtype=int)
 print(d)
 print(d[1,2])
@@ -20,3 +20,17 @@ print(d[3][1]) #same as below
 print(d[3,1]) # same as above line
 print(d[1:3],)
 print(d[1:3,1:3])
+array1=np.arange(1,12)
+print (array1)
+array2=np.array([[1,2,3,4,5,6],[7,8,9,10,11,12]], dtype=int)
+print(array1+array1) 
+#print(array1+array2) # cannot add two different shaped arrays. shape is dimension.
+print(np.shape(array2)) 
+array3= np.array(["abc","xyz", "fgb"], dtype=str)
+print(array3[0])
+print(array3[0][0])
+array4= np.array([["abc","xyz", "fgb"],], dtype=str) ; #double square brackets did the magic to replace string
+print(array4[0][0])
+x='xy'
+array4[0][0]=x
+print (array4)
